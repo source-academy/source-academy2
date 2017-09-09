af4ed4c31940de988381cb21aecf32dcb114bab9
Evan Sebastian <evanlhoini@gmail.com>
2017-09-09 16:32:28 +0700

    (front-office) Add feature to play any story when logged in as staff

--- frontend/src/story-xml/index.js
+++ frontend/src/story-xml/index.js
@@ -4,7 +4,7 @@ var axios = require('axios')
 var StoryXMLPlayer = require('./story-xml-player');
 var createInitializer = require('./create-initializer');
 var container = document.getElementById('game-display')
-var story = container.getAttribute('data-story')
+var story = window.STORY_OVERRIDE || container.getAttribute('data-story')
 var username = container.getAttribute('data-username')
 var attemptedAll = container.getAttribute('data-attempted-all') === "true"
 var initialize = createInitializer(StoryXMLPlayer, story, username, attemptedAll)
--- lib/source_academy_web/controllers/page_controller.ex
+++ lib/source_academy_web/controllers/page_controller.ex
@@ -17,20 +17,29 @@ defmodule SourceAcademyWeb.PageController do
     end
   end
 
-  def game(conn, _params) do
+  def game(conn, params) do
+    user = conn.assigns.current_user
     student = conn.assigns.current_student
     assessment = Assessments.prepare_game(student)
+    story_override =
+      if Accounts.staff?(user) do
+        Map.get(params, "s") || nil
+      else
+        nil
+      end
     if assessment == nil do
       render conn, "game.html",
         assessment_id: -1,
         story: "spaceship",
         username: student.user.first_name,
+        story_override: story_override,
         attempted_all: true
     else
       render conn, "game.html",
         assessment_id: assessment.id,
         story: Assessments.story_name(assessment),
         username: student.user.first_name,
+        story_override: story_override,
         attempted_all: false
     end
   end

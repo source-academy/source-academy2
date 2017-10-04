098c0138cfdf0d7641305ee01bfa8baa21e8fb19
Evan Sebastian <evan.sebastian@grab.com>
2017-10-04 08:48:44 +0800

    (frontend-toolchain) Add symbols for Week 8

--- frontend/src/toolchain/createContext.ts
+++ frontend/src/toolchain/createContext.ts
@@ -119,6 +119,11 @@ export const importBuiltins = (context: Context) => {
   if (context.week >= 6) {
     defineSymbol(context, 'is_number', misc.is_number)
   }
+  if (context.week >= 8) {
+    defineSymbol(context, 'undefined', undefined)
+    defineSymbol(context, 'set_head', list.set_head)
+    defineSymbol(context, 'set_head', list.set_tail)
+  }
 }
 
 const createContext = (week = 3, externals = []) => {
